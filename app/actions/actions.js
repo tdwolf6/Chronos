1482dfdd223b5bab68c23cfa208e305e70a7f1a3
tdwolf6 <rodrickbuckner@gmail.com>
2020-07-11 11:15:39 -0700

    created actoins and actiontypes file folders

---
+++ app/actions/actions.js
@@ -0,0 +1,12 @@
+// import actionType constants
+import * as types from '../types/actionTypes';
+// export const (action) = (param)=>;
+
+// export const action = data => {
+//   return {
+//     type: types.action,
+//     payload: data,
+//   };
+// };
+
+// test
---
+++ app/types/actionTypes.js
@@ -0,0 +1,6 @@
+// add action type constants i.e.:
+// export const ACTION_DESCRIPTION = "ACTION_DESCRIPTION";
+
+// export const ACTION = "ACTION"
+
+// test
